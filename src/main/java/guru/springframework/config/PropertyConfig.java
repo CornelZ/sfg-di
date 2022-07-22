caffba23c6dac3b0f3409b4102258e2c6ac2fb5d
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-22 13:04:02 +0300

    os environment properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,15 +1,19 @@
 package guru.springframework.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+  @Autowired Environment env;
 
   @Value("${guru.user}")
   String user;
@@ -23,7 +27,7 @@ public class PropertyConfig {
   @Bean
   FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
-    fakeDataSource.setUser(user);
+    fakeDataSource.setUser(env.getProperty("GURU_USERNAME"));
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
     return fakeDataSource;
